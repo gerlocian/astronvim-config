4326038ff245d2e6ad3c1c00d153cde98892151c
Patrick Ortiz <portiz@OTIS-DESKTOP>
2023-04-18 00:58:30 -0400

    Updated nvim astronvim settings

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,22 +1,27 @@
+-- Mason handles external editor tooling including language servers, linters, formatters, debuggers, etc.
+
 -- customize mason plugins
 return {
   -- use mason-lspconfig to configure LSP installations
   {
+    -- Handles language servers such as code completion and language diagnostics.
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = { "lua_ls" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
+    -- Similar to LSP Config, except it's meant for tools that don't have a language server.
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = { "stylua" },
     },
   },
   {
+    -- Debuggers
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
---
+++ plugins/todo-comments.lua
@@ -0,0 +1,18 @@
+-- TODO: Make sure you check this in.
+
+return {
+  {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+        -- my config goes here
+      }
+    end,
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
+}
